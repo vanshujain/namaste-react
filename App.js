b386626e4e7fa50ae2d43ca3543ab5af6bbfaaac
vanshika jain <vanshu23121996.jain@gmail.com>
2023-12-29 19:25:37 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,20 @@
+const heading = React.createElement(
+    "h1", 
+    {id:"heading", xyz: "abc"}, 
+    "Hello World from React"
+    );
+    
+    const parent = React.createElement("div", {id:"parent"},[
+        React.createElement("div",{id:"child"},[
+            React.createElement("h1", {}, "Iam an h1 tag"),
+            React.createElement("h2", {}, "I am an h2 tag"),
+        ]),
+                React.createElement("div",{id:"child2"},[
+            React.createElement("h1", {}, "Iam an h1 tag"),
+            React.createElement("h2", {}, "I am an h2 tag"),
+                ])
+    ])
+    
+    console.log(heading,parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
